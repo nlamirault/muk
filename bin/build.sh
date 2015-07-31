6b5194387b791721533005a32a6bb8c808ebd81a
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-07-31 15:30:56 +0200

    Update build
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -19,21 +19,33 @@ OK_COLOR="\033[32;01m"
 ERROR_COLOR="\033[31;01m"
 WARN_COLOR="\033[33;01m"
 
-MOUNT="$HOME/mnt"
-ISO="iso"
-GRUB="grub"
+MOUNT="/mnt"
+DIR=`pwd`
+ISO="${DIR}/iso"
+GRUB="${DIR}/grub"
+
+if [ -z "$1" ]; then
+	echo "Need parameter.: $0 sdb1"
+	exit 1
+fi
+
 DEV="/dev/$1"
 
-echo -e "${OK_COLOR}[muk] Use ${DEV} for USB Key${NO_COLOR}"
+echo -e "${OK_COLOR}[muk] Use ${DEV}${NO_COLOR}"
+mount ${DEV} ${MOUNT}/muk
 
 echo -e "${OK_COLOR}[muk] Initialize key${NO_COLOR}"
 mkdir -p ${MOUNT}/muk/{boot,iso}
-# mkfs.vfat -n MUK ${DEV}
-# mount ${DEV} ${MOUNT}/muk
 
 echo -e "${OK_COLOR}[muk] Install GRUB${NO_COLOR}"
-# grub-install --no-floppy --root-directory=${MOUNT}/muk ${DEV}
+grub-install --force --no-floppy --boot-directory=${MOUNT}/muk/boot ${DEV}
+grub-install --force --no-floppy --boot-directory=/mnt/muk/boot /dev/sdb
+echo -e "${OK_COLOR}[muk] Install Iso and Boot${NO_COLOR}"
+touch ${MOUNT}/muk/boot/grub/grub.cfg
+# cp -r ${GRUB} ${MOUNT}/muk/boot/
+# rsync -auvzh --exclude '*.txt' --progress ${ISO} ${MOUNT}/muk/
 
-echo -e "${OK_COLOR}[muk] Install GRUB${NO_COLOR}"
-# cp ${ISO}/*.iso ${MOUNT}/muk/iso
-cp -r ${GRUB} ${MOUNT}/muk/boot/
+echo -e "${OK_COLOR}[muk] Finish installation${NO_COLOR}"
+umount ${MOUNT}/muk
+
+echo -e "${OK_COLOR}[muk] Done${NO_COLOR}"
