ade7008458d9ba729f2dd459cafdb187d93a48c4
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-07-19 18:03:06 +0200

    FIX build usb key
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2015 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# Copyright (C) 2015, 2016 Nicolas Lamirault <nicolas.lamirault@gmail.com>
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,33 +19,46 @@ OK_COLOR="\033[32;01m"
 ERROR_COLOR="\033[31;01m"
 WARN_COLOR="\033[33;01m"
 
-MOUNT="/mnt"
+
+if [ -z "$1" ]; then
+    echo -e "${ERROR_COLOR}[muk] device mount directory missing.${NO_COLOR}"
+    echo -e "Usage:\t $0 /mnt sdb1 ${NO_COLOR}"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo -e "${ERROR_COLOR}[muk] device parameter missing.${NO_COLOR}"
+    echo -e "Usage:\t $0 /mnt sdb1 ${NO_COLOR}"
+    exit 1
+fi
+
 DIR=`pwd`
+
 ISO="${DIR}/iso"
 GRUB="${DIR}/grub"
 
-if [ -z "$1" ]; then
-	echo "Need parameter.: $0 sdb1"
-	exit 1
-fi
+MOUNT=$1
+DEV=$2
 
-DEV="/dev/$1"
+echo -e "${OK_COLOR}[muk] Build using ${MOUNT} ${DEV}${NO_COLOR}"
 
-echo -e "${OK_COLOR}[muk] Use ${DEV}${NO_COLOR}"
-mount ${DEV} ${MOUNT}/muk
+# echo -e "${OK_COLOR}[muk] Initialize directories${NO_COLOR}"
+# sudo mkdir -p ${MOUNT}/boot/iso
 
-echo -e "${OK_COLOR}[muk] Initialize key${NO_COLOR}"
-mkdir -p ${MOUNT}/muk/{boot,iso}
+# echo -e "${OK_COLOR}[muk] Install GRUB${NO_COLOR}"
+# sudo grub-install --force --no-floppy --boot-directory=/run/media/nlamirault/MUK/boot /dev/sdb
 
-echo -e "${OK_COLOR}[muk] Install GRUB${NO_COLOR}"
-grub-install --force --no-floppy --boot-directory=${MOUNT}/muk/boot ${DEV}
-grub-install --force --no-floppy --boot-directory=/mnt/muk/boot /dev/sdb
-echo -e "${OK_COLOR}[muk] Install Iso and Boot${NO_COLOR}"
-touch ${MOUNT}/muk/boot/grub/grub.cfg
-# cp -r ${GRUB} ${MOUNT}/muk/boot/
-# rsync -auvzh --exclude '*.txt' --progress ${ISO} ${MOUNT}/muk/
 
-echo -e "${OK_COLOR}[muk] Finish installation${NO_COLOR}"
-umount ${MOUNT}/muk
+# # sudo grub-install --directory=/usr/lib/grub/i386-pc --boot-directory=${MOUNT}/boot/ /dev/${DEV}
+# # # sudo grub-install --force --no-floppy --boot-directory=${MOUNT}/boot ${DEV}
+# # # sudo grub-install --removable --no-floppy --target x86_64-efi --boot-directory=${MOUNT}/boot ${DEV}
+# # # grub2-install --directory=/usr/lib/grub/i386-pc --boot-directory=${USBMNT:-/mnt}/boot /dev/${USBDEV}
+
+
+# # echo -e "${OK_COLOR}[muk] Install Boot options${NO_COLOR}"
+# # sudo cp -r ${GRUB} ${MOUNT}/boot/
+
+echo -e "${OK_COLOR}[muk] Install ISOs${NO_COLOR}"
+rsync -auvzhP --exclude '*.txt' --progress ${ISO} ${MOUNT}/boot/
 
 echo -e "${OK_COLOR}[muk] Done${NO_COLOR}"
